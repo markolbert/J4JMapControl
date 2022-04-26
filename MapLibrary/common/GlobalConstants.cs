60108f7c0d92e182e3ead4c9cf44bc960d3f5868
Mark Olbert <mark@arcabama.com>
2022-04-26 14:00:02 -0700

    define defaults

---
+++ MapLibrary/common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace J4JSoftware.MapLibrary;
+
+public static class GlobalConstants
+{
+    public const int MaxDetailLevel = 23;
+    public const int EarthRadius = 6378137;
+    public const double Wgs84MaxLatitude = 85.05112878;
+    public const int BingMapsMaxDetailLevel = 23;
+}
---
+++ MapLibrary/common/MapSourceParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace J4JSoftware.MapLibrary;
+
+public record MapSourceParameters(
+    double MaxLatitude = GlobalConstants.Wgs84MaxLatitude,
+    int MaxDetailLevel = GlobalConstants.MaxDetailLevel,
+    int EarthRadius = GlobalConstants.EarthRadius
+) : IMapSourceParameters;
