a297fe382037df2d35f0bec644a294d196e97c1e
Mark Olbert <mark@arcabama.com>
2022-04-30 12:26:28 -0700

    define tile attached property for Image controls

---
+++ MapLibraryWinApp/AttachedProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace J4JSoftware.MapLibrary
+{
+    public static class AttachedProperties
+    {
+        public static readonly DependencyProperty TileCoordinatesProperty =
+            DependencyProperty.RegisterAttached( "TileCoordinates",
+                                                 typeof( Coordinates ),
+                                                 typeof( Image ),
+                                                 null );
+
+        public static Coordinates GetTileCoordinates( Image bitmapSource ) =>
+            (Coordinates) bitmapSource.GetValue( TileCoordinatesProperty );
+
+        public static void SetTileCoordinates( Image bitmapSource, Coordinates value ) =>
+            bitmapSource.SetValue( TileCoordinatesProperty, value );
+    }
+}
