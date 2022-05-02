046667955c31ddf5610ec1f3a049177e9e07b79f
Mark Olbert <mark@arcabama.com>
2022-05-02 10:39:08 -0700

    attached property asserting an Image is a map Image

--- MapLibraryWinApp/AttachedProperties.cs
+++ MapLibraryWinApp/AttachedProperties.cs
@@ -12,15 +12,30 @@ namespace J4JSoftware.MapLibrary
     public static class AttachedProperties
     {
         public static readonly DependencyProperty TileCoordinatesProperty =
-            DependencyProperty.RegisterAttached( "TileCoordinates",
+            DependencyProperty.RegisterAttached( "TileCoordinatesProperty",
                                                  typeof( Coordinates ),
                                                  typeof( Image ),
                                                  null );
 
-        public static Coordinates GetTileCoordinates( Image bitmapSource ) =>
-            (Coordinates) bitmapSource.GetValue( TileCoordinatesProperty );
+        public static Coordinates? GetTileCoordinates( Image image ) =>
+            image.GetValue( TileCoordinatesProperty ) as Coordinates;
 
-        public static void SetTileCoordinates( Image bitmapSource, Coordinates value ) =>
-            bitmapSource.SetValue( TileCoordinatesProperty, value );
+        public static void SetTileCoordinates( Image image, Coordinates value ) =>
+            image.SetValue( TileCoordinatesProperty, value );
+
+        public static readonly DependencyProperty IsMapTileProperty = DependencyProperty.RegisterAttached(
+            "IsMapTileProperty",
+            typeof( bool ),
+            typeof( Image ),
+            null );
+
+        public static bool GetIsMapTile( Image image )
+        {
+            var propValue = image.GetValue( IsMapTileProperty );
+
+            return propValue != null && (bool) propValue;
+        }
+
+        public static void SetIsMapTile( Image image, bool value ) => image.SetValue( IsMapTileProperty, value );
     }
 }
