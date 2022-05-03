2e410b707c771f823895a38ec2e9a82fb0b3f3ee
Mark Olbert <mark@arcabama.com>
2022-05-03 15:57:13 -0700

    add utility method for extracting coordinates

--- MapLibraryWinApp/PublicExtensions.cs
+++ MapLibraryWinApp/PublicExtensions.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Microsoft.UI.Xaml.Controls;
 
 namespace J4JSoftware.MapLibrary;
 
 public static class PublicExtensions
 {
+    public static List<Coordinates> ExtractCoordinates( this IEnumerable<Image> images )
+    {
+        var retVal = new List<Coordinates>();
+
+        foreach( var image in images )
+        {
+            var coordinates = AttachedProperties.GetTileCoordinates( image );
+            if( coordinates != null )
+                retVal.Add( coordinates );
+        }
+
+        return retVal;
+    }
 }
