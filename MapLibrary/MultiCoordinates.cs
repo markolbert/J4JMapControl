b4e8e8559580448807e472092db4b4ff3669a4a0
Mark Olbert <mark@arcabama.com>
2022-05-10 17:35:27 -0700

    reorganize coordinate classes, eliminate unneeded ones

---
+++ MapLibrary/MultiCoordinates.cs
@@ -0,0 +1,28 @@
+﻿namespace J4JSoftware.MapLibrary;
+
+public record MultiCoordinates
+{
+    public MultiCoordinates(
+        TilePoint tilePoint,
+        IMapProjection mapProjection,
+        CoordinateOrigin origin
+    )
+    {
+        TilePoint = tilePoint;
+
+        ScreenPoint = new DoublePoint( tilePoint.X * mapProjection.TileWidthHeight,
+                                       tilePoint.Y * mapProjection.TileWidthHeight,
+                                       origin,
+                                       mapProjection );
+
+        LatLong = new LatLong( mapProjection.MapRetrieverInfo )
+        {
+            Latitude = mapProjection.ScreenToLatitude( ScreenPoint ),
+            Longitude = mapProjection.ScreenToLongitude( ScreenPoint )
+        };
+    }
+
+    public LatLong LatLong { get; }
+    public TilePoint TilePoint { get; }
+    public DoublePoint ScreenPoint { get; }
+}
