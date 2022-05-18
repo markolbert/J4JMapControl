bef400d9e1b7e88604401c8d55c6bc1caa636a92
Mark Olbert <mark@arcabama.com>
2022-05-18 09:28:24 -0400

    remove separate map tile section

--- Test.MapLibraryWinApp/ViewModel.cs
+++ Test.MapLibraryWinApp/ViewModel.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using Windows.Foundation;
 using J4JSoftware.J4JMapControl;
 using J4JSoftware.Logging;
 using J4JSoftware.MapLibrary;
@@ -17,13 +16,8 @@ public class ViewModel : ObservableObject
     private readonly IJ4JLogger _logger;
 
     private SelectableItem<IMapImageRetriever>? _selectedRetriever;
-    private int _xTile;
-    private int _yTile;
     private int _zoomLevel;
-    private HorizontalBinder _horizBinder;
-    private VerticalBinder _vertBinder;
-
-    private BitmapImage? _tileImageSource;
+    private Size _mapSize;
     private string? _errorMsg;
     private bool _suppressUpdate;
 
@@ -50,18 +44,6 @@ public class ViewModel : ObservableObject
         }
 
         Retrievers = temp;
-
-        HorizontalBinders = Enum.GetValues<SmallMapHorizontalAlignment>()
-                                .Select( x => new HorizontalBinder( x ) )
-                                .ToList();
-
-        HorizontalBinder = HorizontalBinders.First( x => x.Name.Equals( "Center", StringComparison.OrdinalIgnoreCase ) );
-
-        VerticalBinders = Enum.GetValues<SmallMapVerticalAlignment>()
-                              .Select( x => new VerticalBinder( x ) )
-                              .ToList();
-
-        VerticalBinder = VerticalBinders.First(x => x.Name.Equals("Middle", StringComparison.OrdinalIgnoreCase));
     }
 
     public List<SelectableItem<IMapImageRetriever>> Retrievers { get; }
@@ -90,53 +72,15 @@ public class ViewModel : ObservableObject
 
             OnPropertyChanged(nameof(MinZoom));
             OnPropertyChanged(nameof(MaxZoom));
-            OnPropertyChanged(nameof(MaxTile));
 
             _suppressUpdate = false;
 
             ZoomLevel = MinZoom;
-
-            UpdateTileMap();
         }
     }
 
     public int MinZoom => _selectedRetriever?.Item.MapRetrieverInfo.MinimumZoom ?? 0;
     public int MaxZoom => _selectedRetriever?.Item.MapRetrieverInfo.MaximumZoom ?? 0;
-    public int MaxTile => _selectedRetriever?.Item.MapProjection.ZoomFactor ?? 0;
-
-    public int XTile
-    {
-        get => _xTile;
-
-        set
-        {
-            if (_suppressUpdate)
-                return;
-
-            _suppressUpdate = true;
-            SetProperty(ref _xTile, value);
-            _suppressUpdate = false;
-
-            UpdateTileMap();
-        }
-    }
-
-    public int YTile
-    {
-        get => _yTile;
-
-        set
-        {
-            if (_suppressUpdate)
-                return;
-
-            _suppressUpdate = true;
-            SetProperty(ref _yTile, value);
-            _suppressUpdate = false;
-
-            UpdateTileMap();
-        }
-    }
 
     public int ZoomLevel
     {
@@ -153,89 +97,13 @@ public class ViewModel : ObservableObject
 
             if( _selectedRetriever != null )
                 _selectedRetriever.Item.MapProjection.ZoomLevel = _zoomLevel;
-
-            UpdateTileMap();
         }
     }
 
-    public List<HorizontalBinder> HorizontalBinders { get; }
-
-    public HorizontalBinder HorizontalBinder
+    public Size MapSize
     {
-        get => _horizBinder;
-
-        set
-        {
-            if( _suppressUpdate )
-                return;
-
-            _suppressUpdate = true;
-            SetProperty( ref _horizBinder, value );
-            _suppressUpdate = false;
-        }
-    }
-
-    public List<VerticalBinder> VerticalBinders { get; }
-
-    public VerticalBinder VerticalBinder
-    {
-        get => _vertBinder;
-
-        set
-        {
-            if( _suppressUpdate )
-                return;
-
-            _suppressUpdate = true;
-            SetProperty( ref _vertBinder, value );
-            _suppressUpdate = false;
-        }
-    }
-
-    private void UpdateTileMap()
-    {
-        ErrorMessage = null;
-        TileImageSource = null;
-
-        if( Location == null
-        || _selectedRetriever?.Item.MapRetrieverInfo is not {} _ )
-            return;
-
-        var tile = new MultiCoordinates( new TilePoint( _xTile, _yTile, ZoomLevel ),
-                                         _selectedRetriever.Item.MapProjection,
-                                         CoordinateOrigin.UpperLeft );
-
-        _selectedRetriever.Item.GetMapImageAsync( tile )
-                          .ContinueWith( t =>
-                           {
-                               _dQueue.TryEnqueue( () =>
-                               {
-                                   if( t.Result is not { ReturnValue: MapImageData imageData } )
-                                   {
-                                       ErrorMessage = "Failed to retrieve image";
-                                       return;
-                                   }
-
-                                   var newImage = new BitmapImage();
-                                   imageData.Stream.Seek( 0 );
-                                   newImage.SetSource( imageData.Stream );
-
-                                   TileImageSource = newImage;
-                               } );
-                           } );
-    }
-
-    public BitmapImage? TileImageSource
-    {
-        get => _tileImageSource;
-
-        set
-        {
-            SetProperty( ref _tileImageSource, value );
-
-            OnPropertyChanged( nameof( TileHeight ) );
-            OnPropertyChanged( nameof( TileWidth ) );
-        }
+        get => _mapSize;
+        set => SetProperty(ref _mapSize, value);
     }
 
     public string? ErrorMessage
@@ -243,9 +111,6 @@ public class ViewModel : ObservableObject
         get => _errorMsg;
         set => SetProperty( ref _errorMsg, value );
     }
-
-    public int TileWidth=>TileImageSource == null ? 0 : TileImageSource.PixelWidth;
-    public int TileHeight => TileImageSource == null ? 0 : TileImageSource.PixelHeight;
 
     public LatLong? Location
     {
