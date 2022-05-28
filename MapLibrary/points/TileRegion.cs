127701dcc795795cbe8af252b7e92d43b59a7c23
Mark Olbert <mark@arcabama.com>
2022-05-28 13:49:52 -0700

    define tile region

--- MapLibrary/points/TileRegion.cs
+++ MapLibrary/points/TileRegion.cs
@@ -1,3 +1,36 @@
-﻿namespace J4JSoftware.MapLibrary;
+﻿using System.Reflection.Metadata.Ecma335;
+using Windows.Foundation;
 
-public record TileRegion(TilePoint UpperLeft, TilePoint LowerRight);
+namespace J4JSoftware.MapLibrary;
+
+public record TileRegion( MapTile UpperLeft, MapTile LowerRight )
+{
+    public int HorizontalTiles => LowerRight.X - UpperLeft.X + 1;
+    public int VerticalTiles => LowerRight.Y - UpperLeft.Y + 1;
+
+    public bool IsInRegion( MapTile tile )
+    {
+        if( tile.X < UpperLeft.X || tile.X > LowerRight.X )
+            return false;
+
+        if( tile.Y < UpperLeft.Y || tile.Y > LowerRight.Y )
+            return false;
+
+        return true;
+    }
+
+    public Point GetUpperLeft( IMapProjection mapProjection ) =>
+        mapProjection.ToScreenPoint( UpperLeft );
+
+    public Point GetLowerRight( IMapProjection mapProjection ) =>
+        mapProjection.ToScreenPoint( LowerRight );
+
+    // upper left corner origin
+    public Rect GetProjectionRect( IMapProjection mapProjection ) =>
+        new( GetUpperLeft( mapProjection ),
+                  new Size( HorizontalTiles * mapProjection.TileWidthHeight,
+                            VerticalTiles * mapProjection.TileWidthHeight ) );
+
+    public LatLong GetCenterLatLong( IMapProjection mapProjection ) =>
+        mapProjection.ScreenToLatLong( GetProjectionRect( mapProjection ).Center() );
+}
