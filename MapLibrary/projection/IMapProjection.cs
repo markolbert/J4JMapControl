585ad5af7fef955841f2ff89ac83abbf5e3fbbef
Mark Olbert <mark@arcabama.com>
2022-05-29 08:59:07 -0700

    remove unneeded methods

--- MapLibrary/projection/IMapProjection.cs
+++ MapLibrary/projection/IMapProjection.cs
@@ -25,13 +25,6 @@ public interface IMapProjection
 
     BoundingBox GetBoundingBox( LatLong center, double viewportWidth, double viewportHeight, double rotation );
 
-    double LatitudeToCartesian( double latitude );
-    double LongitudeToCartesian( double longitude );
-    double[] LatLongToCartesian( LatLong latLong );
-    double CartesianToLatitude( double y );
-    double CartesianToLongitude( double x );
-    LatLong CartesianToLatLong( double x, double y );
-
     Point LatLongToScreen(LatLong latLong);
     LatLong ScreenToLatLong( Point screenPoint );
     LatLong Offset( LatLong origin, double xOffset, double yOffset );
