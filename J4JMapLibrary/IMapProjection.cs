7432ecdaad013dee7586b8129b779c3f9d29614a
Mark Olbert <mark@arcabama.com>
2023-01-24 10:35:13 -0800

    tweak/expand interfaces

--- J4JMapLibrary/IMapProjection.cs
+++ J4JMapLibrary/IMapProjection.cs
@@ -8,14 +8,7 @@ public interface IMapProjection
     double MaxLongitude { get; }
     double MinLongitude { get; }
 
-    int MinX { get; }
-    int MaxX { get; }
-    int MinY { get; }
-    int MaxY { get; }
-    
-    int MaxScale { get; }
-    int MinScale { get; }
-
-    (double latitude, double longitude) ConvertToLatLong(int x, int y);
-    (int x, int y) ConvertToXY(double latitude, double longitude);
+    MapPoint CreateMapPoint();
+    (double latitude, double longitude) XYToLatLong(int x, int y);
+    (int x, int y) LatLongToXY(double latitude, double longitude);
 }
