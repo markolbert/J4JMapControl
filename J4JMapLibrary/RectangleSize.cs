be4910f8586864992418061605d4ea805733bb5f
Mark Olbert <mark@arcabama.com>
2023-01-24 10:36:23 -0800

    add classes to clarify data structures

---
+++ J4JMapLibrary/RectangleSize.cs
@@ -0,0 +1,19 @@
+﻿namespace J4JMapLibrary;
+
+public class RectangleSize
+{
+    private int _width;
+    private int _height;
+
+    public int Width
+    {
+        get => _width; 
+        set => _width = value < 0 ? 0 : value;
+    }
+
+    public int Height
+    {
+        get => _height;
+        set => _height = value < 0 ? 0 : value;
+    }
+}
