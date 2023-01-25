e988a8923ed2b99baca4d7f81350a086365b9aec
Mark Olbert <mark@arcabama.com>
2023-01-24 19:50:50 -0800

    extract static items

---
+++ J4JMapLibrary/MapConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace J4JMapLibrary;
+
+public static class MapConstants
+{
+    public const double TwoPi = Math.PI * 2;
+    public const double HalfPi = Math.PI / 2;
+    public const double QuarterPi = Math.PI / 4;
+    public const double RadiansPerDegree = Math.PI / 180;
+    public const double DegreesPerRadian = 180 / Math.PI;
+    public const double EarthCircumferenceMeters = 6378137;
+    public const double MetersPerInch = 0.0254;
+}
---
+++ J4JMapLibrary/MapExtensions.cs
@@ -0,0 +1,22 @@
+﻿using J4JSoftware.Logging;
+
+namespace J4JMapLibrary;
+
+internal static class MapExtensions
+{
+    internal static T ConformValueToRange<T>(T toCheck, T min, T max, string name, IJ4JLogger logger)
+        where T : IComparable<T>
+    {
+        if (toCheck.CompareTo(min) < 0)
+        {
+            logger.Warning("{0} ({1}) < minimum ({2}), capping", name, toCheck, min);
+            return min;
+        }
+
+        if (toCheck.CompareTo(max) <= 0)
+            return toCheck;
+
+        logger.Warning("{0} ({1}) > maximum ({2}), capping", name, toCheck, max);
+        return max;
+    }
+}
