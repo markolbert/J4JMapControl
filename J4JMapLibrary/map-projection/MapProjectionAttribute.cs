1181eb8532531260ec146e7db79d6d0bde3e7a0d
Mark Olbert <mark@arcabama.com>
2023-01-26 09:34:33 -0800

    add AutoFac support

---
+++ J4JMapLibrary/map-projection/MapProjectionAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace J4JMapLibrary;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited=false)]
+public class MapProjectionAttribute : Attribute
+{
+    public MapProjectionAttribute(
+        string name
+    )
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
