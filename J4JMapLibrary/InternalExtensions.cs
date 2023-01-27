08a07b108e9677fec4b692f0e873358bff7c1680
Mark Olbert <mark@arcabama.com>
2023-01-26 20:22:57 -0800

    rename file for clarity

---
+++ J4JMapLibrary/InternalExtensions.cs
@@ -0,0 +1,23 @@
+﻿using J4JSoftware.Logging;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace J4JMapLibrary;
+
+internal static class InternalExtensions
+{
+    internal static T ConformValueToRange<T>(T toCheck, T min, T max, string name, IJ4JLogger logger)
+        where T : IComparable<T>
+    {
+        if (toCheck.CompareTo(min) < 0)
+        {
+            logger.Warning("{0} ({1}) < minimum ({2}), capping", name, toCheck, min);
+            return min;
+        }
+
+        if (toCheck.CompareTo(max) <= 0)
+            return toCheck;
+
+        logger.Warning("{0} ({1}) > maximum ({2}), capping", name, toCheck, max);
+        return max;
+    }
+}
