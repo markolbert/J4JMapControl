1d2ae1983bb77f878ee3beb9bf7b10fd77c13b31
Mark Olbert <mark@arcabama.com>
2023-01-29 09:45:34 -0800

    conform to revised API

--- MapLibTests/MiscTests.cs
+++ MapLibTests/MiscTests.cs
@@ -6,16 +6,20 @@ namespace MapLibTests;
 public class MiscTests
 {
 
-    [Theory]
-    [InlineData("0", 1, 0, 0)]
-    [InlineData("01", 2, 1, 0)]
-    [InlineData("032", 3, 2, 3)]
-    public void DecodeQuadKeys(string quadKey, int scale, int x, int y)
+    [ Theory ]
+    [ InlineData( "0", 1, 0, 0 ) ]
+    [ InlineData( "01", 2, 1, 0 ) ]
+    [ InlineData( "032", 3, 2, 3 ) ]
+    public void DecodeQuadKeys( string quadKey, int scale, int x, int y )
     {
-        var (calcScale, xTile, yTile) = MapExtensions.ToTileCoordinates(quadKey);
+        MapExtensions.TryParseQuadKey( quadKey, out var deconstructed )
+                     .Should()
+                     .BeTrue();
 
-        calcScale.Should().Be(scale);
-        xTile.Should().Be(x);
-        yTile.Should().Be(y);
+        deconstructed.Should().NotBeNull();
+
+        deconstructed!.Scale.Should().Be( scale );
+        deconstructed.XTile.Should().Be( x );
+        deconstructed.YTile.Should().Be( y );
     }
 }
