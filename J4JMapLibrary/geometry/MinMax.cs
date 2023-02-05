2fd1ba73a793a446bce54a7aec0159049a2db8e8
Mark Olbert <mark@arcabama.com>
2023-02-04 20:22:04 -0800

    add IEqualityComparer

--- J4JMapLibrary/geometry/MinMax.cs
+++ J4JMapLibrary/geometry/MinMax.cs
@@ -3,6 +3,35 @@
 public record MinMax<T>
     where T : struct, IComparable
 {
+    #region IEqualityComparer...
+
+    private sealed class MinimumMaximumEqualityComparer : IEqualityComparer<MinMax<T>>
+    {
+        public bool Equals( MinMax<T>? x, MinMax<T>? y )
+        {
+            if( ReferenceEquals( x, y ) )
+                return true;
+            if( ReferenceEquals( x, null ) )
+                return false;
+            if( ReferenceEquals( y, null ) )
+                return false;
+            if( x.GetType() != y.GetType() )
+                return false;
+
+            return x.Minimum.Equals( y.Minimum )
+             && x.Maximum.Equals( y.Maximum );
+        }
+
+        public int GetHashCode( MinMax<T> obj )
+        {
+            return HashCode.Combine( obj.Minimum, obj.Maximum );
+        }
+    }
+
+    public static IEqualityComparer<MinMax<T>> DefaultComparer { get; } = new MinimumMaximumEqualityComparer();
+
+    #endregion
+
     public MinMax(
         T min,
         T max
