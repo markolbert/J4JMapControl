4ab5d6901b422bf52bdd97ce068c4ac5e66014df
Mark Olbert <mark@arcabama.com>
2023-02-05 18:00:13 -0800

    test data via inlinedata

--- MapLibTests/TileTests.cs
+++ MapLibTests/TileTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Runtime.CompilerServices;
-using FluentAssertions;
+﻿using FluentAssertions;
 using J4JMapLibrary;
 using J4JMapLibrary.Viewport;
 using J4JSoftware.DeusEx;
@@ -11,30 +9,49 @@ namespace MapLibTests;
 public class TileTests : TestBase
 {
     [Theory]
-    [MemberData(nameof(TileDataSource.GetTestData), "BingMaps", MemberType = typeof(TileDataSource))]
-    public async Task BingMapsTileRegion( TileDataSource.Data item )
+    [InlineData("BingMaps", 1, 0, 0, 128, 256, 0, 0, 0, 1, 1)]
+    [InlineData("BingMaps", 2, 37, -122, 128, 256, 0, 0, 2, 1, 2)]
+    [InlineData("BingMaps", 2, 37, -122, 128, 256, 45, 0, 1, 1, 2)]
+    [InlineData("BingMaps", 2, 37, -122, 128, 512, 45, 0, 1, 1, 3)]
+    public async Task BingMapsTileRegion(
+        string projectionName, 
+        int scale, 
+        float latitude, 
+        float longitude, 
+        int height,
+        int width, 
+        float heading, 
+        int minTileX, 
+        int minTileY, 
+        int maxTileX, 
+        int maxTileY)
     {
-        var projection = await GetFactory().CreateMapProjection( item.ProjectionName ) as ITiledProjection;
+        var projection = await GetFactory().CreateMapProjection(projectionName) as ITiledProjection;
         projection.Should().NotBeNull();
         projection!.Initialized.Should().BeTrue();
 
-        projection.SetScale(item.Scale);
+        projection.SetScale(scale);
 
-        var test = J4JDeusEx.ServiceProvider.GetService<ViewportRectangle>();
+        var test = J4JDeusEx.ServiceProvider.GetService<Viewport>();
         test.Should().NotBeNull();
 
-        var tiles = await test!.Projection( projection )
-            .Center( item.CenterLatitude, item.CenterLongitude )
-                               .Height( item.Height )
-                               .Width( item.Width )
-                               .Heading( item.Heading )
-                               .GetViewportTilesAsync( GetCancellationToken( 500 ), true );
+        var tiles = await test!.Projection(projection)
+            .Center(latitude, longitude)
+            .Height(height)
+            .Width(width)
+            .Heading(heading)
+            .GetViewportTilesAsync(GetCancellationToken(500), true);
 
         tiles.Should().NotBeNull();
-        tiles!.TryGetBounds( out var bounds ).Should().BeTrue();
-        bounds!.Should().Be( item.TileBounds );
+        tiles!.TryGetBounds(out var bounds).Should().BeTrue();
 
-        await foreach( var tile in tiles!.GetTilesAsync( projection, GetCancellationToken( 500 ) ) )
+        var testBounds = new TileBounds(
+                new TileCoordinates(minTileX, minTileY), 
+                new TileCoordinates(maxTileX, maxTileY));
+
+        bounds!.Should().Be(testBounds);
+
+        await foreach (var tile in tiles.GetTilesAsync(projection, GetCancellationToken(500)))
         {
             tile.ImageBytes.Should().BeNegative();
         }
