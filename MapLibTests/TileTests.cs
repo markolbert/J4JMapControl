148b48ceec6f3bd3f4d7654f6de85241a376462d
Mark Olbert <mark@arcabama.com>
2023-02-05 20:11:16 -0800

    expand tile tests

--- MapLibTests/TileTests.cs
+++ MapLibTests/TileTests.cs
@@ -10,9 +10,14 @@ public class TileTests : TestBase
 {
     [Theory]
     [InlineData("BingMaps", 1, 0, 0, 128, 256, 0, 0, 0, 1, 1)]
+    [InlineData("OpenStreetMaps", 0, 0, 0, 128, 256, 0, 0, 0, 0, 0)]
     [InlineData("BingMaps", 2, 37, -122, 128, 256, 0, 0, 2, 1, 2)]
     [InlineData("BingMaps", 2, 37, -122, 128, 256, 45, 0, 1, 1, 2)]
     [InlineData("BingMaps", 2, 37, -122, 128, 512, 45, 0, 1, 1, 3)]
+    [InlineData("BingMaps", 3, 37, -122, 512, 512, 45, 0, 3, 2, 6)]
+    [InlineData("BingMaps", 3, 37, -122, 512, 512, 75, 0, 3, 2, 6)]
+    [InlineData("BingMaps", 3, 37, 97, 512, 512, 75, 4, 3, 7, 6)]
+    [InlineData("BingMaps", 3, 37, 97, 512, 512, 0, 5, 3, 7, 5)]
     public async Task BingMapsTileRegion(
         string projectionName, 
         int scale, 
