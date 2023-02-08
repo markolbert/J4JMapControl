c69f8aa18fb3be03cd4723699725beda4c201987
Mark Olbert <mark@arcabama.com>
2023-02-07 19:58:58 -0800

    restrict to just string credentials

---
+++ J4JMapLibrary/config/IProjectionCredentials.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace J4JMapLibrary;
+
+public interface IProjectionCredentials
+{
+    List<Credential> Credentials { get; set; }
+
+    bool TryGetCredential( string name, out string? result );
+}
---
+++ J4JMapLibrary/config/ProjectionCredentials.cs
@@ -0,0 +1,14 @@
+﻿namespace J4JMapLibrary;
+
+public class ProjectionCredentials : IProjectionCredentials
+{
+    public List<Credential> Credentials { get; set; } = new();
+
+    public bool TryGetCredential(string name, out string? result)
+    {
+        result = Credentials
+           .FirstOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase))?.Key;
+
+        return result != null;
+    }
+}
