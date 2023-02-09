4d6128bf1294efb204a31836df15c54ef597ab70
Mark Olbert <mark@arcabama.com>
2023-02-09 12:36:02 -0800

    begin adding google maps

---
+++ J4JMapLibrary/google/GoogleMapType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace J4JMapLibrary;
+
+public enum GoogleMapType
+{
+    [ Description( "Google (Roadmap)" ) ]
+    RoadMap,
+
+    [ Description( "Google (Satellite)" ) ]
+    Satellite,
+
+    [ Description( "Google (Terrain)" ) ]
+    Terrain,
+
+    [ Description( "Google (Hybrid)" ) ]
+    Hybrid
+}
