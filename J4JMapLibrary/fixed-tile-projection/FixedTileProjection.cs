37b06b2ece01146458706b777d1032943d4e062c
Mark Olbert <mark@arcabama.com>
2023-02-09 19:27:06 -0800

    move viewport tile retrieval into projection

--- J4JMapLibrary/fixed-tile-projection/FixedTileProjection.cs
+++ J4JMapLibrary/fixed-tile-projection/FixedTileProjection.cs
@@ -1,4 +1,5 @@
 ﻿using J4JSoftware.Logging;
+using System.Numerics;
 
 namespace J4JMapLibrary;
 
@@ -56,6 +57,98 @@ public abstract class FixedTileProjection<TScope, TAuth> : MapProjection<TScope,
 
     public string MapScale( float latitude, float dotsPerInch ) =>
         $"1 : {GroundResolution( latitude ) * dotsPerInch / MapConstants.MetersPerInch}";
+
+    public async Task<List<FixedMapTile>?> GetViewportRegionAsync(
+        FixedTileViewport viewportData,
+        bool deferImageLoad = false,
+        CancellationToken ctx = default
+    )
+    {
+        var tileList = await GetViewportTilesAsync( viewportData, deferImageLoad, ctx );
+
+        if (tileList == null)
+            return null;
+
+        return await tileList.GetTilesAsync(this, ctx)
+                             .ToListAsync(ctx);
+    }
+
+    public async Task<MapTileList?> GetViewportTilesAsync(
+        FixedTileViewport viewportData,
+        bool deferImageLoad = false,
+        CancellationToken ctx = default
+    )
+    {
+        if( !Initialized )
+        {
+            Logger.Error("Projection not initialized");
+            return null;
+        }
+
+        viewportData = viewportData.Constrain( Scope );
+
+        var cartesianCenter = new Cartesian(Scope);
+        cartesianCenter.SetCartesian(
+            Scope.LatLongToCartesian( viewportData.CenterLatitude, viewportData.CenterLongitude ) );
+
+        var corner1 = new Vector3( cartesianCenter.X - viewportData.Width / 2,
+                                   cartesianCenter.Y + viewportData.Height / 2,
+                                   0 );
+        var corner2 = new Vector3(corner1.X + viewportData.Width, corner1.Y, 0);
+        var corner3 = new Vector3(corner2.X, corner2.Y - viewportData.Height, 0);
+        var corner4 = new Vector3(corner1.X, corner3.Y, 0);
+
+        var corners = new[] { corner1, corner2, corner3, corner4 };
+
+        var vpCenter = new Vector3(cartesianCenter.X, cartesianCenter.Y, 0);
+
+        // apply rotation if one is defined
+        // heading == 270 is rotation == 90, hence the angle adjustment
+        if (viewportData.Heading != 0)
+        {
+            corners = corners.ApplyTransform(
+                Matrix4x4.CreateRotationZ((360 - viewportData.Heading) * MapConstants.RadiansPerDegree, vpCenter));
+        }
+
+        // find the range of tiles covering the mapped rectangle
+        var minTileX = CartesianToTile(corners.Min(x => x.X));
+        var maxTileX = CartesianToTile(corners.Max(x => x.X));
+
+        // figuring out the min/max of y coordinates is a royal pain in the ass...
+        // because in display space, increasing y values take you >>down<< the screen,
+        // not up the screen. So the first adjustment is to subject the raw Y values from
+        // the height of the projection to reverse the direction. 
+        var minTileY = CartesianToTile(corners.Min(y => Height - y.Y));
+        var maxTileY = CartesianToTile(corners.Max(y => Height - y.Y));
+
+        minTileX = minTileX < 0 ? 0 : minTileX;
+        minTileY = minTileY < 0 ? 0 : minTileY;
+
+        var maxTiles = Height / MapServer.TileHeightWidth - 1;
+        maxTileX = maxTileX > maxTiles ? maxTiles : maxTileX;
+        maxTileY = maxTileY > maxTiles ? maxTiles : maxTileY;
+
+        var retVal = new MapTileList();
+
+        for (var xTile = minTileX; xTile <= maxTileX; xTile++)
+        {
+            for (var yTile = minTileY; yTile <= maxTileY; yTile++)
+            {
+                var mapTile = await FixedMapTile.CreateAsync(this, xTile, yTile, ctx: ctx);
+
+                if (!deferImageLoad)
+                    await mapTile.GetImageAsync(ctx: ctx);
+
+                if (!retVal.Add(mapTile))
+                    Logger.Error("Problem adding FixedMapTile to collection (probably differing ITileScope)");
+            }
+        }
+
+        return retVal;
+    }
+
+    private int CartesianToTile(float value) =>
+        Convert.ToInt32(Math.Floor(value / MapServer.TileHeightWidth));
 
     // thanx to 3dGrabber for this
     // https://stackoverflow.com/questions/383587/how-do-you-do-integer-exponentiation-in-c
---
+++ J4JMapLibrary/fixed-tile-projection/FixedTileViewport.cs
@@ -0,0 +1,28 @@
+﻿namespace J4JMapLibrary;
+
+public class FixedTileViewport
+{
+    private float _heading;
+
+    public float CenterLatitude { get; set; }
+    public float CenterLongitude { get; set; }
+    public float Height { get; set; }
+    public float Width { get; set; }
+
+    // in degrees; north is 0/360; stored as mod 360
+    public float Heading
+    {
+        get => _heading;
+        set => _heading = value % 360;
+    }
+
+    public FixedTileViewport Constrain( TileScope scope ) =>
+        new FixedTileViewport
+        {
+            CenterLatitude = scope.LatitudeRange.ConformValueToRange( CenterLatitude, "Latitude" ),
+            CenterLongitude = scope.LongitudeRange.ConformValueToRange( CenterLongitude, "Longitude" ),
+            Height = Height,
+            Width = Width,
+            Heading = Heading
+        };
+}
