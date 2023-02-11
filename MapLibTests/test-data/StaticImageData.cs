2ed825335cb2e6c8d8a84e027efc6b33e6849263
Mark Olbert <mark@arcabama.com>
2023-02-11 09:14:57 -0800

    generate test data as objects

---
+++ MapLibTests/test-data/StaticImageData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace MapLibTests;
+
+public class StaticImageData : IEnumerable<object[]>
+{
+    public record Region( float Latitude, float Longitude, int Scale, float Height, float Width, string FileId );
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { new Region(0,0,0, 512, 512, "000HW512") };
+        yield return new object[] { new Region(0,0,2, 512, 512, "002HW512") };
+        yield return new object[] { new Region( 37, -122, 5, 512, 512,"37N122W5HW512") };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
