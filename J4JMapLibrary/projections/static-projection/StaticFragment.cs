564b391f68eb11e47543b798e65fe5a7ef01a680
Mark Olbert <mark@arcabama.com>
2023-02-11 09:14:41 -0800

    include scale in id

--- J4JMapLibrary/projections/static-projection/StaticFragment.cs
+++ J4JMapLibrary/projections/static-projection/StaticFragment.cs
@@ -20,7 +20,7 @@ public class StaticFragment : MapFragment, IStaticFragment
         Scale = scale;
     }
 
-    protected override string TileId => $"(Lat: {Center.Latitude}, Long: {Center.Longitude})";
+    protected override string FragmentId => $"(Lat: {Center.Latitude}, Long: {Center.Longitude}, Scale: {Scale})";
 
     public LatLong Center { get; }
     public float Height { get; }
