04405b84cd062770d416caa5b63446192e5cec8a
Mark Olbert <mark@arcabama.com>
2023-02-11 09:14:57 -0800

    generate test data as objects

--- MapLibTests/test-data/TileImageData.cs
+++ MapLibTests/test-data/TileImageData.cs
@@ -4,11 +4,13 @@ namespace MapLibTests;
 
 public class TileImageData : IEnumerable<object[]>
 {
+    public record Tile(int Scale, int TileX, int TileY);
+
     public IEnumerator<object[]> GetEnumerator()
     {
-        yield return new object[] { 1, 0, 0 };
-        yield return new object[] { 2, 0, 0 };
-        yield return new object[] { 5, 27, 27 };
+        yield return new object[] { new Tile(1, 0, 0) };
+        yield return new object[] { new Tile(2, 0, 0) };
+        yield return new object[] { new Tile(5, 27, 27) };
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
