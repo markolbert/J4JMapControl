5d433a8368998127d1384b1e404badad00983dd4
Mark Olbert <mark@arcabama.com>
2023-02-14 12:36:45 -0800

    remove unneeded code

--- J4JMapLibrary/extract/MapExtractNg.Configuration.cs
+++ J4JMapLibrary/extract/MapExtractNg.Configuration.cs
@@ -10,7 +10,7 @@ public partial class MapExtractNg
         float Height,
         float Width,
         Buffer Buffer
-    ) 
+    )
     {
         public virtual bool Equals( Configuration? other )
         {
@@ -28,9 +28,8 @@ public partial class MapExtractNg
              && Buffer.Equals( other.Buffer );
         }
 
-        public override int GetHashCode() => HashCode.Combine( Latitude, Longitude, Heading, Scale, Height, Width, Buffer );
-
-        public static readonly Configuration Default = new( 0, 0, 0, 0, 0, 0, Buffer.Default );
+        public override int GetHashCode() =>
+            HashCode.Combine( Latitude, Longitude, Heading, Scale, Height, Width, Buffer );
 
         public bool IsValid( IProjection projection ) =>
             Height > 0 && Width > 0 && Scale >= projection.MapServer.MinScale && Scale <= projection.MapServer.MaxScale;
