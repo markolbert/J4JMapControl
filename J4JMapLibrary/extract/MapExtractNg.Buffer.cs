277407342b7aeb28dbc85c54e04fc489514f53cb
Mark Olbert <mark@arcabama.com>
2023-02-14 09:52:45 -0800

    extract to partial, add IEquatable

---
+++ J4JMapLibrary/extract/MapExtractNg.Buffer.cs
@@ -0,0 +1,22 @@
+﻿namespace J4JMapLibrary;
+
+public partial class MapExtractNg
+{
+    private record Buffer( float HeightPercent, float WidthPercent )
+    {
+        public virtual bool Equals( Buffer? other )
+        {
+            if( ReferenceEquals( null, other ) )
+                return false;
+            if( ReferenceEquals( this, other ) )
+                return true;
+
+            return HeightPercent.Equals( other.HeightPercent )
+             && WidthPercent.Equals( other.WidthPercent );
+        }
+
+        public override int GetHashCode() => HashCode.Combine( HeightPercent, WidthPercent );
+
+        public static readonly Buffer Default = new( 0, 0 );
+    }
+}
