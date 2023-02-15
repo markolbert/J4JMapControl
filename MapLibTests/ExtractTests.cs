a5089a3a32e8c5101e64f4e7ffb19924e1789718
Mark Olbert <mark@arcabama.com>
2023-02-15 09:08:24 -0800

    add extractor tests

--- MapLibTests/ExtractTests.cs
+++ MapLibTests/ExtractTests.cs
@@ -11,7 +11,26 @@ namespace MapLibTests;
 public class ExtractTests : TestBase
 {
     [ Theory ]
-    [InlineData("BingMaps", 0,0,0,256,256,0,1,4)]
+    [InlineData("BingMaps", 0, 0, 0, 256, 256, 0, 1, 4)]
+    [InlineData("BingMaps", 37, -122, 0, 20, 20, 0, 1, 1)]
+    [InlineData("BingMaps", 37, -122, 0, 40, 80, 0, 4, 1)]
+    [InlineData("BingMaps", 40, -122, 315, 40, 200, 0, 4, 2)]
+    [InlineData("BingMaps", 40, -112, 315, 100, 200, 0, 4, 4)]
+    [InlineData("OpenStreetMaps", 0, 0, 0, 256, 256, 0, 1, 4)]
+    [InlineData("OpenStreetMaps", 37, -122, 0, 20, 20, 0, 1, 1)]
+    [InlineData("OpenStreetMaps", 37, -122, 0, 40, 80, 0, 4, 1)]
+    [InlineData("OpenStreetMaps", 40, -122, 315, 40, 200, 0, 4, 2)]
+    [InlineData("OpenStreetMaps", 40, -112, 315, 100, 200, 0, 4, 4)]
+    [InlineData("OpenTopoMaps", 0, 0, 0, 256, 256, 0, 1, 4)]
+    [InlineData("OpenTopoMaps", 37, -122, 0, 20, 20, 0, 1, 1)]
+    [InlineData("OpenTopoMaps", 37, -122, 0, 40, 80, 0, 4, 1)]
+    [InlineData("OpenTopoMaps", 40, -122, 315, 40, 200, 0, 4, 2)]
+    [InlineData("OpenTopoMaps", 40, -112, 315, 100, 200, 0, 4, 4)]
+    [InlineData("GoogleMaps", 0, 0, 0, 256, 256, 0, 1, 1)]
+    [InlineData("GoogleMaps", 37, -122, 0, 20, 20, 0, 1, 1)]
+    [InlineData("GoogleMaps", 37, -122, 0, 40, 80, 0, 4, 1)]
+    [InlineData("GoogleMaps", 40, -122, 315, 40, 200, 0, 4, 1)]
+    [InlineData("GoogleMaps", 40, -112, 315, 100, 200, 0, 4, 1)]
     public async Task BasicExtract(
         string projectionName,
         float latitude,
