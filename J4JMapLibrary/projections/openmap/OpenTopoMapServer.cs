c670692817d8ace3fdb27ecd30e025d9c0e350dd
Mark Olbert <mark@arcabama.com>
2023-02-15 09:08:00 -0800

    seal to support virtual method in ctor, set scale range

--- J4JMapLibrary/projections/openmap/OpenTopoMapServer.cs
+++ J4JMapLibrary/projections/openmap/OpenTopoMapServer.cs
@@ -1,12 +1,13 @@
 ﻿namespace J4JMapLibrary;
 
 [ MapServer( "OpenTopoMaps", typeof( string ) ) ]
-public class OpenTopoMapServer : OpenMapServer, IOpenMapServer
+public sealed class OpenTopoMapServer : OpenMapServer, IOpenMapServer
 {
     public OpenTopoMapServer()
     {
         MinScale = 0;
         MaxScale = 15;
+        ScaleRange = new MinMax<int>(MinScale, MaxScale);
         MaxRequestLatency = 5000;
         RetrievalUrl = "https://tile.opentopomap.org/{zoom}/{x}/{y}.png";
         Copyright = "© OpenTopoMap-Mitwirkende, SRTM | Kartendarstellung\n© OpenTopoMap\nCC-BY-SA";
