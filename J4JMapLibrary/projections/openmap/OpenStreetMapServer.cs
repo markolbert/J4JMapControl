3064e9031fba4ee8d65d5ce236bcc35f0dd8cf45
Mark Olbert <mark@arcabama.com>
2023-02-15 09:08:00 -0800

    seal to support virtual method in ctor, set scale range

--- J4JMapLibrary/projections/openmap/OpenStreetMapServer.cs
+++ J4JMapLibrary/projections/openmap/OpenStreetMapServer.cs
@@ -1,12 +1,13 @@
 ﻿namespace J4JMapLibrary;
 
 [ MapServer( "OpenStreetMaps", typeof( string ) ) ]
-public class OpenStreetMapServer : OpenMapServer, IOpenMapServer
+public sealed class OpenStreetMapServer : OpenMapServer, IOpenMapServer
 {
     public OpenStreetMapServer()
     {
         MinScale = 0;
         MaxScale = 20;
+        ScaleRange = new MinMax<int>( MinScale, MaxScale );
         RetrievalUrl = "https://tile.openstreetmap.org/{zoom}/{x}/{y}.png";
         Copyright = "© OpenStreetMap Contributors";
         CopyrightUri = new Uri( "http://www.openstreetmap.org/copyright" );
