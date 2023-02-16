a85416fdc1009e70b1891ef7ab8c3f15fa089f07
Mark Olbert <mark@arcabama.com>
2023-02-16 11:03:32 -0800

    consolidate code in single file

--- J4JMapLibrary/projections/tiled-projection/TiledFragment.cs
+++ J4JMapLibrary/projections/tiled-projection/TiledFragment.cs
@@ -17,8 +17,29 @@
 
 namespace J4JSoftware.J4JMapLibrary;
 
-public partial class TiledFragment : MapFragment, ITiledFragment
+public class TiledFragment : MapFragment, ITiledFragment
 {
+    // ignoreCaches prevents loops when creating tiles from within the caching system
+    public static async Task<TiledFragment> CreateAsync(
+        ITiledProjection projection,
+        int x,
+        int y,
+        bool ignoreCache = false,
+        CancellationToken ctx = default
+    )
+    {
+        if (projection.TileCache == null || ignoreCache)
+            return new TiledFragment(projection, x, y);
+
+        var entry = await projection.TileCache.GetEntryAsync(projection,
+                                                             x,
+                                                             y,
+                                                             projection.TiledScale!.Scale,
+                                                             ctx: ctx);
+
+        return entry != null ? entry.Tile : new TiledFragment(projection, x, y);
+    }
+
     // internal to hopefully avoid stack overflow
     internal TiledFragment(
         ITiledProjection projection,
