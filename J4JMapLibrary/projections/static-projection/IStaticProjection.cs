6425aa3fab1fa116d4e4913cff1a9bf7382ed3d1
Mark Olbert <mark@arcabama.com>
2023-02-16 08:04:41 -0800

    remove unneeded code

--- J4JMapLibrary/projections/static-projection/IStaticProjection.cs
+++ J4JMapLibrary/projections/static-projection/IStaticProjection.cs
@@ -2,15 +2,15 @@
 
 public interface IStaticProjection : IProjection
 {
-    Task<StaticExtract?> GetExtractAsync(
-        INormalizedViewport viewportData,
-        bool deferImageLoad = false,
-        CancellationToken ctx = default
-    );
+    //Task<StaticFragment?> GetExtractAsync(
+    //    INormalizedViewport viewportData,
+    //    bool deferImageLoad = false,
+    //    CancellationToken ctx = default
+    //);
 }
 
-public interface IStaticProjection<out TScope> : IStaticProjection
-    where TScope : ProjectionScale
-{
-    TScope Scope { get; }
-}
+//public interface IStaticProjection<out TScope> : IStaticProjection
+//    where TScope : ProjectionScale
+//{
+//    TScope Scope { get; }
+//}
--- J4JMapLibrary/projections/tiled-projection/ITiledProjection.cs
+++ J4JMapLibrary/projections/tiled-projection/ITiledProjection.cs
@@ -10,15 +10,9 @@ public interface ITiledProjection : IProjection
     MinMax<int> TileXRange { get; }
     MinMax<int> TileYRange { get; }
 
-    ITiledScale TiledScale { get; }
+    ITiledScale? TiledScale { get; }
     ITileCache? TileCache { get; }
 
     float GroundResolution( float latitude );
     string ScaleDescription( float latitude, float dotsPerInch );
-
-    Task<TiledExtract?> GetExtractAsync(
-        IViewport viewportData,
-        bool deferImageLoad = false,
-        CancellationToken ctx = default
-    );
 }
