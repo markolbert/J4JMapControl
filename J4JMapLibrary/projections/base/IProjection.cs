e67949c0dece307b62e871c9c164ce57fbef4b0d
Mark Olbert <mark@arcabama.com>
2023-02-16 08:03:33 -0800

    generalize viewport specification

--- J4JMapLibrary/projections/base/IProjection.cs
+++ J4JMapLibrary/projections/base/IProjection.cs
@@ -7,13 +7,27 @@ public interface IProjection
     bool Initialized { get; }
 
     IMapServer MapServer { get; }
-    ProjectionScale MapScale { get; }
+    IProjectionScale MapScale { get; }
 
     Task<bool> AuthenticateAsync( object? credentials, CancellationToken ctx = default );
+
+    IAsyncEnumerable<IMapFragment> GetExtractAsync(
+        INormalizedViewport viewportData,
+        bool deferImageLoad = false,
+        CancellationToken ctx = default
+    );
 }
 
-public interface IProjection<in TAuth> : IProjection
+public interface IProjection<in TAuth, in TViewport, out TFrag> : IProjection
     where TAuth : class
+    where TViewport : INormalizedViewport
+    where TFrag : IMapFragment
 {
     Task<bool> AuthenticateAsync( TAuth? credentials, CancellationToken ctx = default );
+
+    IAsyncEnumerable<TFrag> GetExtractAsync(
+        TViewport viewportData,
+        bool deferImageLoad = false,
+        CancellationToken ctx = default
+    );
 }
