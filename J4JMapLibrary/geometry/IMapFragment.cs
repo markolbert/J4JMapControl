d5f888bed5c5e04a8834e3b7065daf784a77b2ac
Mark Olbert <mark@arcabama.com>
2023-02-16 08:00:51 -0800

    make imagedata nullable

--- J4JMapLibrary/geometry/IMapFragment.cs
+++ J4JMapLibrary/geometry/IMapFragment.cs
@@ -2,8 +2,11 @@
 
 public interface IMapFragment
 {
+    public string FragmentId { get; }
+
     IMapServer MapServer { get; }
     int MaxRequestLatency { get; }
+    public byte[]? ImageData { get; }
     long ImageBytes { get; }
     event EventHandler? ImageChanged;
     Task<byte[]?> GetImageAsync( int scale, bool forceRetrieval = false, CancellationToken ctx = default );
