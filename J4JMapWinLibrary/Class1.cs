daea17f0dc8f32b3592521ed47a16f1ed4f49537
Mark Olbert <mark@arcabama.com>
2023-02-16 08:38:30 -0800

    setup

--- J4JMapWinLibrary/Class1.cs
+++ J4JMapWinLibrary/Class1.cs
@@ -1,5 +1,19 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
+// Copyright (c) 2021, 2022 Mark A. Olbert 
+// 
+// This file is part of ConsoleUtilities.
+//
+// ConsoleUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// ConsoleUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with ConsoleUtilities. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
@@ -10,7 +24,8 @@ using System.Threading.Tasks;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace J4JMapWinLibrary;
+namespace J4JSoftware.J4JMapWinLibrary;
+
 public class Class1
 {
 }
