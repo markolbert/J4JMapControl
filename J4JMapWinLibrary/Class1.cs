bfe65194a3344fe57a6ebcedf852ef3142b40b75
Mark Olbert <mark@arcabama.com>
2023-02-16 08:28:59 -0800

    add new project

---
+++ J4JMapWinLibrary/Class1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace J4JMapWinLibrary;
+public class Class1
+{
+}
