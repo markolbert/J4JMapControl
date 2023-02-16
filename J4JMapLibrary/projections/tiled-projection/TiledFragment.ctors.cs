608c5b6a865787e585618a267a1fde962c1f8144
Mark Olbert <mark@arcabama.com>
2023-02-16 08:28:51 -0800

    add license headers

--- J4JMapLibrary/projections/tiled-projection/TiledFragment.ctors.cs
+++ J4JMapLibrary/projections/tiled-projection/TiledFragment.ctors.cs
@@ -1,4 +1,21 @@
-﻿namespace J4JMapLibrary;
+﻿// Copyright (c) 2021, 2022 Mark A. Olbert 
+// 
+// This file is part of ConsoleUtilities.
+//
+// ConsoleUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// ConsoleUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with ConsoleUtilities. If not, see <https://www.gnu.org/licenses/>.
+
+namespace J4JMapLibrary;
 
 public partial class TiledFragment : MapFragment
 {
