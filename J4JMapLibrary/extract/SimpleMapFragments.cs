a608936ed2dd34b67586024f1d44e90d81070c7e
Mark Olbert <mark@arcabama.com>
2023-02-16 07:59:10 -0800

    augment map fragments collection

---
+++ J4JMapLibrary/extract/SimpleMapFragments.cs
@@ -0,0 +1,16 @@
+﻿using J4JSoftware.Logging;
+
+namespace J4JMapLibrary;
+
+public class SimpleMapFragments : MapFragments<IMapFragment>
+{
+    private static IMapFragment DefaultFactory( IMapFragment fragment ) => fragment;
+
+    public SimpleMapFragments( 
+        IProjection projection, 
+        IJ4JLogger logger 
+        )
+        : base( projection, DefaultFactory, logger )
+    {
+    }
+}
