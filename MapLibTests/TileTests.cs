cfc6f19485aba19ac6a4e27a0200d256bad6041c
Mark Olbert <mark@arcabama.com>
2023-02-26 15:13:16 -0800

    corrected test results

--- MapLibTests/TileTests.cs
+++ MapLibTests/TileTests.cs
@@ -8,13 +8,13 @@ public class TileTests : TestBase
     [ Theory ]
     [ InlineData( "BingMaps", 1, 0, 0, 128, 256, 0, 0, 0, 1, 1 ) ]
     [ InlineData( "OpenStreetMaps", 0, 0, 0, 128, 256, 0, 0, 0, 0, 0 ) ]
-    [ InlineData( "BingMaps", 2, 37, -122, 128, 256, 0, 0, 2, 1, 2 ) ]
+    [ InlineData( "BingMaps", 2, 37, -122, 128, 256, 0, 0, 1, 1, 1 ) ]
     [ InlineData( "BingMaps", 2, 37, -122, 128, 256, 45, 0, 1, 1, 2 ) ]
-    [ InlineData( "BingMaps", 2, 37, -122, 128, 512, 45, 0, 1, 1, 3 ) ]
-    [ InlineData( "BingMaps", 3, 37, -122, 512, 512, 45, 0, 3, 2, 6 ) ]
-    [ InlineData( "BingMaps", 3, 37, -122, 512, 512, 75, 0, 3, 2, 6 ) ]
-    [ InlineData( "BingMaps", 3, 37, 97, 512, 512, 75, 4, 3, 7, 6 ) ]
-    [ InlineData( "BingMaps", 3, 37, 97, 512, 512, 0, 5, 3, 7, 5 ) ]
+    [ InlineData( "BingMaps", 2, 37, -122, 128, 512, 45, 0, 0, 1, 2 ) ]
+    [ InlineData( "BingMaps", 3, 37, -122, 512, 512, 45, 0, 1, 2, 4 ) ]
+    [ InlineData( "BingMaps", 3, 37, -122, 512, 512, 75, 0, 1, 2, 4 ) ]
+    [ InlineData( "BingMaps", 3, 37, 97, 512, 512, 75, 4, 1, 7, 4 ) ]
+    [ InlineData( "BingMaps", 3, 37, 97, 512, 512, 0, 5, 2, 7, 4 ) ]
     public async Task TileRegion(
         string projectionName,
         int scale,
