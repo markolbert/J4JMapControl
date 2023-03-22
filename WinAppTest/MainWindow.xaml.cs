b368c09b6ca4f839b427f339e21f6b8a0af17d56
Mark Olbert <mark@arcabama.com>
2023-03-21 19:49:44 -0700

    reduce window size for clarity

--- WinAppTest/MainWindow.xaml.cs
+++ WinAppTest/MainWindow.xaml.cs
@@ -4,6 +4,11 @@
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+using Windows.Graphics;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+
 namespace WinAppTest;
 
 /// <summary>
@@ -14,5 +19,11 @@ public sealed partial class MainWindow
     public MainWindow()
     {
         this.InitializeComponent();
+
+        var hWnd = WindowNative.GetWindowHandle(this);
+        var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+
+        appWindow.Resize( new SizeInt32( 800, 800 ) );
     }
 }
