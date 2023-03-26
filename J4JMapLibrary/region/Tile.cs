6276445bcb85534a33d0d41e88d03c020a66f694
Mark Olbert <mark@arcabama.com>
2023-03-25 17:35:47 -0700

    abstract tile coordinates from MapTile functionality

---
+++ J4JMapLibrary/region/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Serilog;
+
+namespace J4JSoftware.J4JMapLibrary.MapRegion;
+
+public class Tile
+{
+    public Tile(
+        MapRegion region,
+        int x,
+        int y
+    )
+    {
+        Region = region;
+        Logger = region.Logger.ForContext<MapTile>();
+
+        ( X, Y ) = region.ProjectionType switch
+        {
+            ProjectionType.Static => ( 0, 0 ),
+            ProjectionType.Tiled => ( x, y ),
+            _ => throw new InvalidEnumArgumentException(
+                $"Unsupported {typeof( ProjectionType )} value '{region.ProjectionType}'" )
+        };
+    }
+
+    protected ILogger Logger { get; }
+
+    public MapRegion Region { get; }
+    public int X { get; }
+    public int Y { get; }
+}
