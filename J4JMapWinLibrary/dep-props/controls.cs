607cb1eda92e3f0088d34bc9c3e8924175cb004d
Mark Olbert <mark@arcabama.com>
2023-04-01 09:39:18 -0700

    create properties related to map controls

---
+++ J4JMapWinLibrary/dep-props/controls.cs
@@ -0,0 +1,96 @@
+using Windows.UI;
+using Microsoft.UI.Xaml;
+
+namespace J4JSoftware.J4JMapWinLibrary;
+
+public sealed partial class J4JMapControl
+{
+    public DependencyProperty VerticalControlAlignmentProperty = DependencyProperty.Register(
+        nameof( VerticalControlAlignment ),
+        typeof( VerticalAlignment ),
+        typeof( J4JMapControl ),
+        new PropertyMetadata( VerticalAlignment.Top ) );
+
+    public VerticalAlignment VerticalControlAlignment
+    {
+        get => (VerticalAlignment) GetValue( VerticalControlAlignmentProperty );
+        set => SetValue( VerticalControlAlignmentProperty, value );
+    }
+
+    public DependencyProperty HorizontalControlAlignmentProperty = DependencyProperty.Register(
+        nameof(HorizontalControlAlignment),
+        typeof(HorizontalAlignment),
+        typeof(J4JMapControl),
+        new PropertyMetadata(HorizontalAlignment.Right));
+
+    public HorizontalAlignment HorizontalControlAlignment
+    {
+        get => (HorizontalAlignment)GetValue(HorizontalControlAlignmentProperty);
+        set => SetValue(HorizontalControlAlignmentProperty, value);
+    }
+
+    public DependencyProperty ControlVisibilityProperty = DependencyProperty.Register(
+        nameof(ControlVisibility),
+        typeof(bool),
+        typeof(J4JMapControl),
+        new PropertyMetadata(true));
+
+    public bool ControlVisibility
+    {
+        get => (bool)GetValue(ControlVisibilityProperty);
+        set => SetValue(ControlVisibilityProperty, value);
+    }
+
+    public DependencyProperty CompassRoseHeightWidthProperty = DependencyProperty.Register(
+        nameof(CompassRoseHeightWidth),
+        typeof(double),
+        typeof(J4JMapControl),
+        new PropertyMetadata(100));
+
+    public double CompassRoseHeightWidth
+    {
+        get => (double)GetValue(CompassRoseHeightWidthProperty);
+        set => SetValue(CompassRoseHeightWidthProperty, value);
+    }
+
+    public DependencyProperty ControlBackgroundProperty = DependencyProperty.Register(
+        nameof(ControlBackground),
+        typeof(Color),
+        typeof(J4JMapControl),
+        new PropertyMetadata(0));
+
+    public Color ControlBackground
+    {
+        get => (Color)GetValue(ControlBackgroundProperty);
+        set => SetValue(ControlBackgroundProperty, value);
+    }
+
+    public DependencyProperty ControlBackgroundOpacityProperty = DependencyProperty.Register(
+        nameof(ControlBackgroundOpacity),
+        typeof(double),
+        typeof(J4JMapControl),
+        new PropertyMetadata(0.6));
+
+    public double ControlBackgroundOpacity
+    {
+        get => (double)GetValue(ControlBackgroundOpacityProperty);
+        set => SetValue(ControlBackgroundOpacityProperty, value);
+    }
+
+    public DependencyProperty LargeMapScaleChangeProperty = DependencyProperty.Register(
+        nameof(LargeMapScaleChange),
+        typeof(double),
+        typeof(J4JMapControl),
+        new PropertyMetadata(0.6));
+
+    public double LargeMapScaleChange
+    {
+        get => (double)GetValue(LargeMapScaleChangeProperty);
+
+        set
+        {
+            value = value <= 0 ? 1 : 0;
+            SetValue( LargeMapScaleChangeProperty, value );
+        }
+    }
+}
