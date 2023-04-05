49413252a44c5134a592ed9832dc4ec16f459e5e
Mark Olbert <mark@arcabama.com>
2023-04-05 14:24:30 -0700

    update docs

--- J4JMapLibrary/region/Tile.cs
+++ J4JMapLibrary/region/Tile.cs
@@ -28,7 +28,7 @@ public class Tile
     public MapRegion Region { get; }
 
     // For Tile objects, X can be any value in the extended tile plane.
-    // for MapTile objects, X can only take on values within the range of
+    // For MapTile objects, X can only take on values within the range of
     // minimum and maximum tile values for the scale of the MapRegion in
     // which the MapTile is created, or -1 if outside the MapRegion's 
     // horizontal limits, giving allowance for wrapping around the left
