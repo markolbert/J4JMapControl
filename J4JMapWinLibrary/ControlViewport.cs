b306f1084722a4084a8ad3c9fb482efcf8fd0301
Mark Olbert <mark@arcabama.com>
2023-04-11 10:43:14 -0700

    remove obsolete code

--- J4JMapWinLibrary/ControlViewport.cs
+++
@@ -1,8 +0,0 @@
-using J4JSoftware.J4JMapLibrary;
-
-namespace J4JSoftware.J4JMapWinLibrary;
-
-public record ControlViewport( string Center, string MapScale, string Heading, Viewport? Viewport )
-{
-    public bool IsValid => Viewport != null;
-}
