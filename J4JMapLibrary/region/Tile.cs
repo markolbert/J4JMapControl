cb1cf92856de48587685d987d6f25dc44c4b2056
Mark Olbert <mark@arcabama.com>
2023-04-13 09:36:15 -0700

    failed to accomodate static projections properly

--- J4JMapLibrary/region/Tile.cs
+++ J4JMapLibrary/region/Tile.cs
@@ -61,10 +61,11 @@ public class Tile
     public int X { get; protected set; }
     public int Y { get; }
 
-    public (int X, int Y) GetUpperLeftCartesian() =>
+    public (float X, float Y) GetUpperLeftCartesian() =>
         Region.ProjectionType switch
         {
-            ProjectionType.Static => ( 0, 0 ),
+            ProjectionType.Static => ( Region.Center.X - Region.BoundingBox.Width / 2,
+                                       Region.Center.Y - Region.BoundingBox.Height / 2 ),
             ProjectionType.Tiled => ( X * Region.Projection.TileHeightWidth, Y * Region.Projection.TileHeightWidth ),
             _ => throw new InvalidEnumArgumentException(
                 $"Unsupported {typeof( ProjectionType )} value '{Region.ProjectionType}'" )
