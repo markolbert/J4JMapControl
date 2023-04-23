e78e544ab069cbcffa358e62cabac94fd79e168f
Mark Olbert <mark@arcabama.com>
2023-04-23 14:13:54 -0700

    move methods to base library

--- J4JMapLibrary/MapExtensions.cs
+++ J4JMapLibrary/MapExtensions.cs
@@ -19,11 +19,16 @@
 // with J4JMapLibrary. If not, see <https://www.gnu.org/licenses/>.
 #endregion
 
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace J4JSoftware.J4JMapLibrary;
 
 public static class MapExtensions
 {
     private static readonly char[] ValidQuadKeyCharacters = { '0', '1', '2', '3' };
+    private static readonly Regex LatLongRegEx = new("^\\s*((-?[0-9]*\\.?)?[0-9]+)(\\D*)$", RegexOptions.Compiled);
+    private static readonly string[] CardinalDirections = { "N", "North", "S", "South", "E", "East", "W", "West" };
 
     public static ProjectionType GetProjectionType( this IProjection projection ) =>
         projection.GetType().GetInterface( nameof( ITiledProjection ) ) == null
@@ -107,5 +112,143 @@ public static class MapExtensions
         var decLog = fraction.IndexOf( '.' );
 
         return ( whole, fraction[ ( decLog + 1 ).. ] );
+    }
+
+    public static bool TryParseLatitudeDirection(string? text, out int sign)
+    {
+        if( !string.IsNullOrEmpty( text ) )
+            text = text.Trim().ToUpper();
+
+        sign = text switch
+        {
+            "N" => 1,
+            "NORTH" => 1,
+            "S" => -1,
+            "SOUTH" => -1,
+            _ => 0
+        };
+
+        return sign != 0;
+    }
+
+    public static bool TryParseLongitudeDirection(string? text, out int sign)
+    {
+        if (!string.IsNullOrEmpty(text))
+            text = text.Trim().ToUpper();
+
+        sign = text switch
+        {
+            "E" => 1,
+            "EAST" => 1,
+            "W" => -1,
+            "WEST" => -1,
+            _ => 0
+        };
+
+        return sign != 0;
+    }
+
+    public static bool TryParseToLatitude( string? text, out float latitude )
+    {
+        latitude = 0;
+
+        if( string.IsNullOrEmpty( text ) )
+            return false;
+
+        var results = LatLongRegEx.Matches( text );
+        if( !results.Any() )
+            return false;
+
+        if( !float.TryParse( results[ 0].Groups[1].Value, out latitude ) )
+            return false;
+
+        latitude = MapConstants.Wgs84LatitudeRange.ConformValueToRange( latitude, "TryParseToLatitude" );
+
+        if( latitude <= 0 || results[0].Groups.Count < 4 )
+            return true;
+
+        if( !TryParseLatitudeDirection( results[ 0 ].Groups[ 3 ].Value, out var sign ) )
+            return string.IsNullOrEmpty( results[ 0 ].Groups[ 3 ].Value );
+
+        latitude *= sign;
+        return true;
+    }
+
+    public static bool TryParseToLongitude(string? text, out float longitude)
+    {
+        longitude = 0;
+
+        if (string.IsNullOrEmpty(text))
+            return false;
+
+        var results = LatLongRegEx.Matches(text);
+        if (!results.Any())
+            return false;
+
+        if (!float.TryParse(results[0].Groups[1].Value, out longitude))
+            return false;
+
+        longitude = MapConstants.LongitudeRange.ConformValueToRange(longitude, "TryParseToLongitude");
+
+        if (longitude <= 0 || results[0].Groups.Count < 4)
+            return true;
+
+        if (!TryParseLongitudeDirection(results[0].Groups[3].Value, out var sign))
+            return string.IsNullOrEmpty(results[0].Groups[3].Value);
+
+        longitude *= sign;
+        return true;
+    }
+
+    public static bool TryParseToLatLong(string? text, out float latitude, out float longitude)
+    {
+        latitude = 0;
+        longitude = 0;
+
+        if (string.IsNullOrEmpty(text))
+            return false;
+
+        var parts = text.Split(new[] { ',' });
+        if (parts.Length != 2)
+            return false;
+
+        return TryParseToLatitude( parts[ 0 ], out latitude ) && TryParseToLongitude( parts[ 1 ], out longitude );
+    }
+
+    public static string ConvertToLatLongText(float latitude, float longitude)
+    {
+        var sb = new StringBuilder();
+        sb.Append(latitude);
+        sb.Append(latitude < 0 ? "S, " : "N, ");
+        sb.Append(longitude);
+        sb.Append(longitude < 0 ? "W" : "E");
+
+        return sb.ToString();
+    }
+
+    public static bool TryParseHeading( string text, out double heading )
+    {
+        heading = text.ToLower().Trim() switch
+        {
+            "n" => 0D,
+            "e" => -90D,
+            "s" => 180D,
+            "w" => 90D,
+            "ne" => -45D,
+            "se" => -135D,
+            "sw" => 135D,
+            "nw" => 45D,
+            "nne" => -22.5,
+            "ene" => -67.5,
+            "ese" => -112.5,
+            "sse" => -157.5,
+            "ssw" => 157.5,
+            "wsw" => 112.5,
+            "wnw" => 67.5,
+            "nnw" => 22.5,
+            _ => double.NaN
+        };
+
+        return !double.IsNaN( heading );
     }
 }
