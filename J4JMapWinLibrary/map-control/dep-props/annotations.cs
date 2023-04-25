797a1089a60e049e949f25d11f38a7a6ea4686b0
Mark Olbert <mark@arcabama.com>
2023-04-25 08:38:42 -0700

    cleanup code, eliminate unneeded code

--- J4JMapWinLibrary/map-control/dep-props/annotations.cs
+++ J4JMapWinLibrary/map-control/dep-props/annotations.cs
@@ -27,25 +27,16 @@ namespace J4JSoftware.J4JMapWinLibrary;
 
 public sealed partial class J4JMapControl
 {
+    private Canvas? _annotationsCanvas;
+
     public static readonly DependencyProperty AnnotationsProperty = DependencyProperty.Register( nameof( Annotations ),
         typeof( List<FrameworkElement> ),
         typeof( J4JMapControl ),
         new PropertyMetadata( new List<FrameworkElement>() ) );
-
-    private Canvas? _annotationsCanvas;
 
     public List<FrameworkElement> Annotations
     {
         get => (List<FrameworkElement>) GetValue( AnnotationsProperty );
         set => SetValue( AnnotationsProperty, value );
     }
-
-    //private void ValidateAnnotations()
-    //{
-    //    foreach( var uiElement in Annotations )
-    //    {
-    //        if( Location.TryParseLatLong( uiElement, out _, out _ ) )
-    //            continue;
-    //    }
-    //}
 }
