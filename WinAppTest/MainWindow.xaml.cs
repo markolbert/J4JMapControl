7ce89dc1d433ae8d4fc7e20f9d62831e0ae0fba8
Mark Olbert <mark@arcabama.com>
2023-05-03 16:17:50 -0700

    resequence code

--- WinAppTest/MainWindow.xaml.cs
+++ WinAppTest/MainWindow.xaml.cs
@@ -157,11 +157,11 @@ public sealed partial class MainWindow
         }
         else
         {
-            var widthInInches = Convert.ToInt32( 8 * monitorInfo.RawDpiX );
-            var heightInInches = Convert.ToInt32( 12 * monitorInfo.RawDpiY );
-
             winSize.Height = monitorInfo.NativeResolutionInRawPixels.Height;
             winSize.Width = monitorInfo.NativeResolutionInRawPixels.Width;
+
+            var widthInInches = Convert.ToInt32(8 * monitorInfo.RawDpiX);
+            var heightInInches = Convert.ToInt32(12 * monitorInfo.RawDpiY);
 
             winSize.Height = winSize.Height > heightInInches? heightInInches: winSize.Height;
             winSize.Width = winSize.Width > widthInInches ? widthInInches: winSize.Width;
