f4c8ceebf6d18aa426b8eaff109a591fd24a1868
Mark Olbert <mark@arcabama.com>
2023-06-17 15:36:33 -0700

    clarify names

--- J4JMapLibrary/region/TileCoordinates.cs
+++ J4JMapLibrary/region/TileCoordinates.cs
@@ -1,16 +1,7 @@
-﻿using System.ComponentModel;
+﻿namespace J4JSoftware.J4JMapLibrary.MapRegion;
 
-namespace J4JSoftware.J4JMapLibrary.MapRegion;
-
-public record TileCoordinates( MapRegion Region, int X, int Y )
+public class TileCoordinates
 {
-    public (float X, float Y) GetUpperLeftCartesian() =>
-        Region.ProjectionType switch
-        {
-            ProjectionType.Static => ( Region.Center.X - Region.BoundingBox.Width / 2,
-                                       Region.Center.Y - Region.BoundingBox.Height / 2 ),
-            ProjectionType.Tiled => ( X * Region.Projection.TileHeightWidth, Y * Region.Projection.TileHeightWidth ),
-            _ => throw new InvalidEnumArgumentException(
-                $"Unsupported {typeof( ProjectionType )} value '{Region.ProjectionType}'" )
-        };
+    public int Row { get; set; }
+    public int Column { get; set; }
 }
