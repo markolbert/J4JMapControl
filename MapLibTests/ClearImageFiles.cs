d5e20e1349c3a548ba9d2c3ec164a2ba33c46ed4
Mark Olbert <mark@arcabama.com>
2023-06-18 18:38:22 -0700

    clear check images before generating them

---
+++ MapLibTests/ClearImageFiles.cs
@@ -0,0 +1,15 @@
+﻿namespace MapLibTests;
+
+public class ClearImageFiles : TestBase
+{
+    public ClearImageFiles()
+    {
+        foreach (var projName in ProjectionNames)
+        {
+            foreach (var file in Directory.GetFiles(GetCheckImagesFolder(projName)))
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
